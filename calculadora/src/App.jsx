f32e297a32dcb138b613f6f8f6ec182b24565f4e
Your Name <you@example.com>
2025-03-25 09:00:40 -0600

    commit

--- calculadora/src/App.jsx
+++ calculadora/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Calculadora from './components/calculadora'
+import Todo from './components/Todo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,12 @@ function App() {
 
       <Calculadora/>
 
+    </div>
+
+    <div>
+      <h1>Todo List</h1>
+
+      <Todo/>
     </div>
     </>
   )
